f8c1b72ac6368883f9e36107c4bed5c77f41b59e
Ciaran Hale <ciaran.hale@optus.com.au>
2014-06-04 21:23:49 +1000

    Doing tutorial

--- src/ReactBlog.js
+++ src/ReactBlog.js
@@ -1,14 +1,59 @@
 /**
- * exported HelloWorld
- *
- * Note how I have the global statement in this comment block - limitation of JSX compiler.
- *
  * @jsx React.DOM
  * */
-var ReactBlog = React.createClass({displayName: 'ReactBlog',
-    render: function () {
-        return (
-          React.DOM.h2(null, 'React ' + (this.props.name || 'Blog') + '!')
-        );
-    }
+ // tutorial8.js
+var data = [
+  {author: "Pete Hunt", text: "This is one comment"},
+  {author: "Jordan Walke", text: "This is *another* comment"}
+];
+// tutorial5.js
+var Comment = React.createClass({
+  render: function() {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        {this.props.children}
+      </div>
+    );
+  }
 });
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+        <Comment author="Pete Hunt">This is one comment</Comment>
+        <Comment author="Jordan Walke">This is *another* comment</Comment>
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
+});
+// tutorial1.js
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+         <h1>Comments</h1>
+        <CommentList data={this.props.data} />
+        <CommentForm />
+      </div>
+    );
+  }
+});
+React.renderComponent(
+  <CommentBox />,
+  document.getElementById('content')
+);
+
+
